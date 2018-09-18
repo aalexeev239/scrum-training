dbe791f9f790177b57f446650b6a79d3eb5bed97
Andrey Alexeev <alexeev.andrey.a@gmail.com>
2018-09-18 23:49:33 +0300

    main files

--- Gruntfile.js
+++ Gruntfile.js
@@ -133,7 +133,7 @@ module.exports = function(grunt) {
 				tasks: 'css-themes'
 			},
 			css: {
-				files: [ 'css/reveal.scss' ],
+				files: [ 'css/reveal.scss', 'css/custom.scss' ],
 				tasks: 'css-core'
 			},
 			html: {
